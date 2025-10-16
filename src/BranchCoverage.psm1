3978ed536ab04af2286631e7f5700041f5117c76
Cédric Belin <cedx@outlook.com>
2025-10-16 18:37:28 +0200

    Add the `FunctionCoverage` and `FunctionData` classes

--- src/BranchCoverage.psm1
+++ src/BranchCoverage.psm1
@@ -1,4 +1,46 @@
+using namespace System.Collections.Generic
 using module ./Tokens.psm1
+
+<#
+.SYNOPSIS
+	Provides the coverage data of branches.
+#>
+class BranchCoverage {
+
+	<#
+	.SYNOPSIS
+		The coverage data.
+	#>
+	[BranchData[]] $Data = @()
+
+	<#
+	.SYNOPSIS
+		The number of branches found.
+	#>
+	[int] $Found = 0
+
+	<#
+	.SYNOPSIS
+		The number of branches hit.
+	#>
+	[int] $Hit = 0
+
+	<#
+	.SYNOPSIS
+		Returns a string representation of this object.
+	.OUTPUTS
+		The string representation of this object.
+	#>
+	[string] ToString() {
+		$lines = [List[string]]::new($this.Data.Length + 2) # TODO ???? [List[string]] $this.Data.Length + 2
+		# TODO ??? [List[string]]::new($this.Data)
+
+		$lines = $this.Data.ForEach("ToString")
+		$lines += "$([Tokens]::BranchesFound):$($this.Found)"
+		$lines += "$([Tokens]::BranchesHit):$($this.Hit)"
+		return $lines -join "`n"
+	}
+}
 
 <#
 .SYNOPSIS
@@ -37,33 +79,7 @@ class BranchData {
 		The string representation of this object.
 	#>
 	[string] ToString() {
-		$value = "$([Tokens]::BranchData):$($this.LineNumber),$($this.BlockNumber),$($this.BranchNumber)";
-		return $this.Taken -gt 0 ? "$value,$($this.Taken)" : "$value,-";
-		return $this.Taken -gt 0 ? "$value,$($this.Taken)" : "$value,-";
+		$value = "$([Tokens]::BranchData):$($this.LineNumber),$($this.BlockNumber),$($this.BranchNumber)"
+		return $this.Taken -gt 0 ? "$value,$($this.Taken)" : "$value,-"
 	}
-}
-
-<#
-.SYNOPSIS
-	Provides the coverage data of branches.
-#>
-class BranchCoverage {
-
-	<#
-	.SYNOPSIS
-		The coverage data.
-	#>
-	[BranchData[]] $Data = @()
-
-	<#
-	.SYNOPSIS
-		The number of branches found.
-	#>
-	[int] $Found = 0
-
-	<#
-	.SYNOPSIS
-		The number of branches hit.
-	#>
-	[int] $Hit = 0
 }
--- src/FunctionCoverage.psm1
+++ src/FunctionCoverage.psm1
@@ -1,1 +1,90 @@
 using module ./Tokens.psm1
+
+<#
+.SYNOPSIS
+	Provides the coverage data of functions.
+#>
+class FunctionCoverage {
+
+	<#
+	.SYNOPSIS
+		The coverage data.
+	#>
+	[FunctionData[]] $Data = @()
+
+	<#
+	.SYNOPSIS
+		The number of functions found.
+	#>
+	[int] $Found = 0
+
+	<#
+	.SYNOPSIS
+		The number of functions hit.
+	#>
+	[int] $Hit = 0
+
+	<#
+	.SYNOPSIS
+		Returns a string representation of this object.
+	.OUTPUTS
+		The string representation of this object.
+	#>
+	[string] ToString() {
+		# $lines = $this.Data.ForEach("ToString")
+		# $lines += "$([Tokens]::FunctionsFound):$($this.Found)"
+		# $lines += "$([Tokens]::FunctionsHit):$($this.Hit)"
+		# return $lines -join "`n"
+		# TODO
+		return ""
+	}
+}
+
+<#
+.SYNOPSIS
+	Provides details for function coverage.
+#>
+class FunctionData {
+
+	<#
+	.SYNOPSIS
+		The execution count.
+	#>
+	[int] $ExecutionCount = 0
+
+	<#
+	.SYNOPSIS
+		The function name.
+	#>
+	[int] $FunctionName = [string]::Empty
+
+	<#
+	.SYNOPSIS
+		The line number of the function start.
+	#>
+	[int] $LineNumber = 0
+
+	<#
+	.SYNOPSIS
+		Returns a string representation of this object.
+	.OUTPUTS
+		The string representation of this object.
+	#>
+	[string] ToString() {
+		return $this.ToString($false)
+	}
+
+	<#
+	.SYNOPSIS
+		Returns a string representation of this object.
+	.PARAMETER $asDefinition
+		Whether to return the function definition instead of its data.
+	.OUTPUTS
+		The string representation of this object.
+	#>
+	[string] ToString([bool] $asDefinition) {
+		$token = $asDefinition ? [Tokens]::FunctionName : [Tokens]::FunctionData
+		$count = $asDefinition ? $this.LineNumber : $this.ExecutionCount
+		return "${token}:$count,$($this.FunctionName)"
+	}
+}
