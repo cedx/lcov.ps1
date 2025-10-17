a76a12cf68557027444f0a43d3d3f95bf4134749
Cédric Belin <cedx@outlook.com>
2025-10-17 11:35:18 +0200

    Add the `Report` class [skip ci]

--- src/Report.psm1
+++ src/Report.psm1
@@ -1,3 +1,4 @@
+using namespace System.Collections.Generic
 using module ./SourceFile.psm1
 using module ./Tokens.psm1
 
@@ -22,10 +23,62 @@ class Report {
 	<#
 	.SYNOPSIS
 		Creates a new report.
-	.PARAMETER $path
-		The path to the source file.
+	.PARAMETER $testName
+		The test name.
 	#>
-	SourceFile([string] $testName) {
+	Report([string] $testName) {
 		$this.TestName = $testName
+		$this.SourceFiles = @()
+	}
+
+	<#
+	.SYNOPSIS
+		Creates a new report.
+	.PARAMETER $testName
+		The test name.
+	.PARAMETER $sourceFiles
+		The source file list.
+	#>
+	Report([string] $testName, [SourceFile[]] $sourceFiles) {
+		$this.TestName = $testName
+		$this.SourceFiles = $sourceFiles
+	}
+
+	<#
+	.SYNOPSIS
+		Parses the specified coverage data in LCOV format.
+	.PARAMETER $coverage
+		The coverage data.
+	.OUTPUTS
+		The resulting coverage report.
+	#>
+	[Report] Parse([string] $coverage) {
+		return $null
+	}
+
+	<#
+	.SYNOPSIS
+		Parses the specified coverage data in LCOV format.
+	.PARAMETER $coverage
+		The coverage data.
+	.OUTPUTS
+		The resulting coverage report, or `$null` if a parsing error occurred.
+	#>
+	[Report] TryParse([string] $coverage) {
+		try { return $this.Parse($coverage) }
+		catch { return $null }
+	}
+
+	<#
+	.SYNOPSIS
+		Returns a string representation of this object.
+	.OUTPUTS
+		The string representation of this object.
+	#>
+	[string] ToString() {
+		$output = [List[string]]::new()
+		if ($this.TestName) { $output.Add("$([Tokens]::TestName):$($this.TestName)") }
+		$output.AddRange($this.SourceFiles)
+		return $output -join "`n"
 	}
 }
