86d7f6d7fa7b2ab1f75b68b8b730aeaa8bc596b0
Cédric Belin <cedx@outlook.com>
2026-04-24 15:41:41 +0200

    Add the `LineCoverage` class

---
+++ src/LineCoverage.psm1
@@ -0,0 +1,46 @@
+using namespace System.Collections.Generic
+using module ./LineData.psm1
+using module ./Tokens.psm1
+
+<#
+.SYNOPSIS
+	Provides the coverage data of lines.
+#>
+[NoRunspaceAffinity()]
+class LineCoverage {
+
+	<#
+	.SYNOPSIS
+		The coverage data.
+	#>
+	[ValidateNotNull()]
+	[List[LineData]] $Data = @()
+
+	<#
+	.SYNOPSIS
+		The number of lines found.
+	#>
+	[ValidateRange("NonNegative")]
+	[int] $Found
+
+	<#
+	.SYNOPSIS
+		The number of lines hit.
+	#>
+	[ValidateRange("NonNegative")]
+	[int] $Hit
+
+	<#
+	.SYNOPSIS
+		Returns a string representation of this object.
+	.OUTPUTS
+		The string representation of this object.
+	#>
+	[string] ToString() {
+		return @(
+			$this.Data.ForEach{ $_.ToString() }
+			"$([Tokens]::LinesFound):$($this.Found)"
+			"$([Tokens]::LinesHit):$($this.Hit)"
+		) -join "\n"
+	}
+}
