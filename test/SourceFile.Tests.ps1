8a76799c5e5b01d973a62115eefc384c7f2bd4b7
Cédric Belin <cedx@outlook.com>
2026-04-24 18:01:42 +0200

    Add the tests of the `SourceFile` class

---
+++ test/SourceFile.Tests.ps1
@@ -0,0 +1,22 @@
+using module ../src/BranchCoverage.psm1
+using module ../src/FunctionCoverage.psm1
+using module ../src/LineCoverage.psm1
+using module ../src/SourceFile.psm1
+
+<#
+.SYNOPSIS
+	Tests the features of the `SourceFile` class.
+#>
+Describe "SourceFile" {
+	Context "ToString" {
+		It "should return a format like 'SF:<path>\nend_of_record'" {
+			[SourceFile]::new("").ToString() | Should -BeExactly "SF:`nend_of_record"
+
+			$sourceFile = [SourceFile] "/home/cedx/lcov.ps1/program.psm1"
+			$sourceFile.Branches = [BranchCoverage]::new()
+			$sourceFile.Functions = [FunctionCoverage]::new()
+			$sourceFile.Lines = [LineCoverage]::new()
+			$sourceFile.ToString() | Should -BeExactly "SF:/home/cedx/lcov.ps1/program.psm1`n$($sourceFile.Functions)`n$($sourceFile.Branches)`n$($sourceFile.Lines)`nend_of_record"
+		}
+	}
+}
