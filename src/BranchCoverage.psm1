b06174328d91486a5dd8b821b43025b2be5e728a
Cédric Belin <cedx@outlook.com>
2026-04-24 15:41:16 +0200

    Add the `BranchCoverage` class

---
+++ src/BranchCoverage.psm1
@@ -0,0 +1,46 @@
+using namespace System.Collections.Generic
+using module ./BranchData.psm1
+using module ./Tokens.psm1
+
+<#
+.SYNOPSIS
+	Provides the coverage data of branches.
+#>
+[NoRunspaceAffinity()]
+class BranchCoverage {
+
+	<#
+	.SYNOPSIS
+		The coverage data.
+	#>
+	[ValidateNotNull()]
+	[List[BranchData]] $Data = @()
+
+	<#
+	.SYNOPSIS
+		The number of branches found.
+	#>
+	[ValidateRange("NonNegative")]
+	[int] $Found
+
+	<#
+	.SYNOPSIS
+		The number of branches hit.
+	#>
+	[ValidateRange("NonNegative")]
+	[int] $Hit
+
+	<#
+	.SYNOPSIS
+		Returns a string representation of this object.
+	.OUTPUTS
+		The string representation of this object.
+	#>
+	[string] ToString() {
+		return @(
+			$this.Data.ForEach{ $_.ToString() }
+			"$([Tokens]::BranchesFound):$($this.Found)"
+			"$([Tokens]::BranchesHit):$($this.Hit)"
+		) -join "\n"
+	}
+}
